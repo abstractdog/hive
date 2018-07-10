3ad7001a96a31fc97c789f9ac65590ea96130bb0
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2018-07-10 17:44:48 +0200

    removed drops

--- standalone-metastore/src/main/sql/oracle/upgrade-3.1.0-to-4.0.0.oracle.sql
+++ standalone-metastore/src/main/sql/oracle/upgrade-3.1.0-to-4.0.0.oracle.sql
@@ -4,4 +4,3 @@ SELECT 'Upgrading MetaStore schema from 3.1.0 to 4.0.0' AS Status from dual;
 UPDATE VERSION SET SCHEMA_VERSION='4.0.0', VERSION_COMMENT='Hive release version 4.0.0' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 3.1.0 to 4.0.0' AS Status from dual;
 
-ALTER TABLE SDS DROP COLUMN IS_STOREDASSUBDIRECTORIES;

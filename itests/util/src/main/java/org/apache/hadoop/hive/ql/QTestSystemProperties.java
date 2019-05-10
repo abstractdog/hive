797680d0fd27a4cd12a83ac71b7e42a3a6a7c9f8
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2019-05-10 11:01:04 +0200

    wip

--- itests/util/src/main/java/org/apache/hadoop/hive/ql/QTestSystemProperties.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/ql/QTestSystemProperties.java
@@ -27,6 +27,7 @@ public class QTestSystemProperties {
   private static final String SYS_PROP_SRC_UDFS = "test.src.udfs";
   private static final String SYS_PROP_VECTORIZATION_ENABLED = "test.vectorization.enabled";
   private static final String SYS_PROP_CHECK_SYNTAX = "test.check.syntax";
+  private static final String SYS_PROP_FORCE_EXCLUSIONS = "test.force.exclusions";
   private static final String SYS_PROP_BUILD_DIR = "build.dir"; // typically target
 
   public static String getTempDir() {
@@ -59,6 +60,10 @@ public class QTestSystemProperties {
 
   public static boolean shouldCheckSyntax() {
     return isTrue(SYS_PROP_CHECK_SYNTAX);
+  }
+
+  public static boolean shouldForceExclusions() {
+    return isTrue(SYS_PROP_FORCE_EXCLUSIONS);
   }
 
   private static boolean isTrue(String propertyName) {
